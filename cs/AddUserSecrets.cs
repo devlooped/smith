c83fc8f661f54124b0d88888a611cf2e462f07f7
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-02 01:57:41 -0300

    Add parameterless AddUserSecrets

---
+++ cs/AddUserSecrets.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Provides configuration extensions for adding user secrets configuration source.
+/// </summary>
+public static class UserSecretsConfigurationExtensions
+{
+    /// <summary>
+    /// <para>
+    /// Adds the user secrets configuration source with the project-specified user secrets ID.
+    /// </para>
+    /// </summary>
+    /// <param name="configuration">The configuration builder.</param>
+    /// <returns>The configuration builder.</returns>
+    public static IConfigurationBuilder AddUserSecrets(this IConfigurationBuilder configuration)
+    {
+        if (!string.IsNullOrEmpty(ThisAssembly.Constants.UserSecretsId))
+            return configuration.AddUserSecrets(ThisAssembly.Constants.UserSecretsId, reloadOnChange: false);
+
+        return configuration;
+    }
+}
