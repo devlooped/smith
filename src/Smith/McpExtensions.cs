efa37384579188ebff20470c80d6e834a4b66414
Daniel Cazzulino <daniel@cazzulino.com>
2025-07-15 16:44:22 -0300

    Add extension to register a tool individually
    This does away with the need to have a type for the individual tools at all.

---
+++ src/Smith/McpExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using ModelContextProtocol.Server;
+
+namespace Smith;
+
+/// <summary>
+/// Usability extensions for working with MCP.
+/// </summary>
+public static class McpExtensions
+{
+    extension(IMcpServerBuilder builder)
+    {
+        /// <summary>
+        /// Registers a specific method as a server tool.
+        /// </summary>
+        public IMcpServerBuilder WithTool(Delegate tool, JsonSerializerOptions? options = null)
+        {
+            builder.Services.AddSingleton(services
+                => McpServerTool.Create(tool, new() { Services = services, SerializerOptions = options }));
+
+            return builder;
+        }
+    }
+}
